48b844bed556bf9ea8cf2af0754e8d0af5b0ab38
IsabelTseng <isabel870310@gmail.com>
2017-08-03 10:05:41 +0800

    設定後台Ｒｏｕｔｅ

---
+++ app/Http/Controllers/AdminPostsController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminPostsController extends Controller
+{
+    public function index()
+    {
+        return view('admin.posts.index');
+    }
+
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+
+    public function edit($id)
+    {
+        $data = ['id' => $id];
+
+        return view('admin.posts.edit', $data);
+    }
+}

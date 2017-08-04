02da24c24afde4f9d0fd3c5c4b6e22325113e006
IsabelTseng <isabel870310@gmail.com>
2017-08-04 14:35:04 +0800

    後台顯示登入者帳號名稱

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -25,7 +25,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+//    protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
@@ -35,5 +35,10 @@ class ResetPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+
+    public function redirectTo()
+    {
+        return route('admin.dashboard.index');
     }
 }

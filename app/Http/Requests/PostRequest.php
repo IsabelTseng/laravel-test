65e1f1eae3d248ee47aaedfbc23a4b030b8acec0
IsabelTseng <isabel870310@gmail.com>
2017-08-04 11:50:41 +0800

    客製化驗證錯誤訊息

--- app/Http/Requests/PostRequest.php
+++ app/Http/Requests/PostRequest.php
@@ -25,8 +25,27 @@ class PostRequest extends FormRequest
     {
         return [
             'title' => 'required|string|max:100',
+//            'sub_title' => 'required',
             'content' => 'required',
             'is_feature' => 'boolean',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'title' => '標題',
+//            'sub_title' => '副標題',
+            'content' => '內容',
+            'is_feature' => '精選'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'title.required' => '請加上標題。',
+            'content.required' => '請加上內容。',
         ];
     }
 }
